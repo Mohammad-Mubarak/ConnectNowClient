68fd7af9da2821d98a6d9fef2f147ffb754002db
Mubarak <beyourbestversion3@gmail.com>
2026-03-05 22:57:15 +0530

    TURN AND STUN

--- components/hooks/useWebRTC.js
+++ components/hooks/useWebRTC.js
@@ -7,14 +7,14 @@ const SOCKET_URL = 'https://connectnow-ctcz.onrender.com'
 const ICE_CONFIG = {
   iceServers: [
     // // ── Google STUN (same network / easy NAT)
-    // { urls: 'stun:stun.l.google.com:19302' },
-    // { urls: 'stun:stun1.l.google.com:19302' },
-    // { urls: 'stun:stun2.l.google.com:19302' },
+    { urls: 'stun:stun.l.google.com:19302' },
+    { urls: 'stun:stun1.l.google.com:19302' },
+    { urls: 'stun:stun2.l.google.com:19302' },
     {
       urls: 'free.expressturn.com:3478',
       username: '000000002088091354',
       credential: 'gUixIGLNMG5XEn07oaVVF0vHNKk=',
-    }
+    },
   ],
   iceCandidatePoolSize: 10,
   bundlePolicy: 'max-bundle',
